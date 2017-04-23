235ff07f85bb5374e99a85c8272ebcccf1044d05
Anurag Kumar <anuragk1991@gmail.com>
2017-04-23 12:25:13 +0530

    db

--- website/music/views.py
+++ website/music/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Album
+from django.http import Http404
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("<h1>This is music directory</h1>")
+    html = ''
+    albums = Album.objects.all()
+    context = {
+        'all_albums': albums
+    }
+
+    return render(request, 'music/index.html',context)
+
+
+def detail(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    
+    return render(request, 'music/detail.html', {'album':album})
